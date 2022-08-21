a7e2f0cd1a09062773d785e114a5328a3183b775
GaboDotCodes <gabedu2000@gmail.com>
2022-08-21 17:52:06 -0500

    Indexz

--- public/src/index.js
+++ public/src/index.js
@@ -3,15 +3,14 @@ import { Router } from '@vaadin/router';
 import { onAuthChanged } from './utils/Firebase/auth';
 import createRouter from './utils/Router/createRouter';
 
-import './views/main-view/main-layout';
-
 window.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
 
   const routes = [
     {
       path: '/',
-      component: 'main-layout',
+      component: 'main-view',
+      action: async () => { await import('./views/main-view/main-view'); },
       children: [
         {
           path: '/users',
@@ -19,9 +18,18 @@ window.addEventListener('DOMContentLoaded', () => {
           action: async () => { await import('./components/users-layout/users-layout'); },
           children: [
             {
+              path: '/',
+              redirect: '/users/register',
+            },
+            {
               path: '/register',
               component: 'users-register',
               action: async () => { await import('./components/users-register/users-register'); },
+            },
+            {
+              path: '/search',
+              component: 'users-search',
+              action: async () => { await import('./components/users-search/users-search'); },
             },
           ],
         },
