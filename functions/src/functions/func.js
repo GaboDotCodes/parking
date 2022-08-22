7c39ae799d09fd44f67708b37c61c7cbc4766229
GaboDotCodes <gabedu2000@gmail.com>
2022-08-22 17:24:24 -0500

    Delete func

--- functions/src/functions/func.js
+++
@@ -1,7 +0,0 @@
-const { appExpress } = require('../utils/appExpress');
-
-appExpress.get('/', async (req, res) => {
-  res.status(200).json({ message: 'TODO MUY CHIMBA X2' });
-});
-
-exports.func = appExpress;
