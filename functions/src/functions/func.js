0e69330e0da68d90b4f84b426eb87feb19249bc4
GaboDotCodes <gabedu2000@gmail.com>
2022-08-21 21:40:32 -0500

    Change

--- functions/src/functions/func.js
+++ functions/src/functions/func.js
@@ -1,8 +1,7 @@
 const { appExpress } = require('../utils/appExpress');
-const { functions } = require('../utils/firebase');
 
-appExpress.post('/', async (req, res) => {
-  res.status(200).json({ message: "TODO MUY CHIMBA X2" });
+appExpress.get('/', async (req, res) => {
+  res.status(200).json({ message: 'TODO MUY CHIMBA X2' });
 });
 
-exports.func = functions.https.onRequest(appExpress);
+exports.func = appExpress;
